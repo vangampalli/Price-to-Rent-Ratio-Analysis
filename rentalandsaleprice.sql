c62a3aa226f490b83915fa2fa2fe62ba1694f051
junki-ogawa <90418741+junki-ogawa@users.noreply.github.com>
2022-01-15 10:17:37 -0700

    fixed with primary key

--- rentalandsaleprice.sql
+++ rentalandsaleprice.sql
@@ -9,12 +9,12 @@ FROM rental_data;
 
 ----------random zip file + rental data file-------------
 CREATE VIEW random_and_rental AS
-SELECT rand_zip.zipcode 
-	, rand_zip.city
-	, rand_zip.county
-	, rand_zip.population
-	, rental_data.avgprice
-	, rental_data.totalrentals
+SELECT rand_zip.zipcode SERIAL PRIMARY KEY
+	, rand_zip.city VARCHAR (30) NOT NULL
+	, rand_zip.county VARCHAR (30) NOT NULL
+	, rand_zip.population INT
+	, rental_data.avgprice INT
+	, rental_data.totalrentals INT
 	, COUNT(*) AS count
 FROM rand_zip
 JOIN rental_data
@@ -34,11 +34,11 @@ FROM random_and_rental
 ----------random zip file + home sale data file-------------
 
 CREATE VIEW random_and_sales AS
-SELECT rand_zip.zipcode
-	, rand_zip.city
-	, rand_zip.county
-	, rand_zip.population
-	, home_prices_df.medianprice
+SELECT rand_zip.zipcode SERIAL PRIMARY KEY
+	, rand_zip.city VARCHAR (30) NOT NULL
+	, rand_zip.county VARCHAR (30) NOT NULL
+	, rand_zip.population INT
+	, home_prices_df.medianprice INT
 	, home_prices_df.activelistingcount, COUNT(*) AS count
 FROM rand_zip
 JOIN home_prices_df
@@ -56,14 +56,14 @@ FROM random_and_sales;
 
 ----------JOIN of random_and_sales and random_and_rental-------------
 -- CREATE VIEW sales_and_rental AS
-SELECT random_and_sales.zipcode
-	, random_and_sales.city
-	, random_and_sales.county
-	, random_and_sales.population
-	, random_and_sales.medianprice
-	, random_and_sales.activelistingcount
-	, random_and_rental.avgprice
-	, random_and_rental.totalrentals
+SELECT random_and_sales.zipcode SERIAL PRIMARY KEY
+	, random_and_sales.city VARCHAR (30) NOT NULL
+	, random_and_sales.county VARCHAR (30) NOT NULL
+	, random_and_sales.population INT
+	, random_and_sales.medianprice INT
+	, random_and_sales.activelistingcount INT
+	, random_and_rental.avgprice INT
+	, random_and_rental.totalrentals INT
 	, COUNT(*) AS count
 FROM random_and_rental
 JOIN random_and_sales
@@ -81,9 +81,9 @@ HAVING COUNT (*) > 1
 
 ------joining rental and sales tables-----
 CREATE VIEW sales_and_rental AS
-select rental_data.zipcode
-	, rental_data.avgprice
-	, home_prices_df.medianprice
+select rental_data.zipcode SERIAL PRIMARY KEY
+	, rental_data.avgprice INT
+	, home_prices_df.medianprice INT
 FROM rental_data
 JOIN home_prices_df
 ON home_prices_df.zipcode = rental_data.zipcode
@@ -96,17 +96,17 @@ FROM sales_and_rental
 
 ----price to rent ratio = median sale/median annual rent
 CREATE VIEW pricetorentratio AS
-SELECT zipcode
+SELECT zipcode SERIAL PRIMARY KEY
 	, sales_and_rental.medianprice/(sales_and_rental.avgprice*12) as ratio
 	, avgprice
 	, medianprice
 FROM sales_and_rental;
 
 CREATE VIEW pricetorent_rounded AS
-select 	zipcode
-	, cast(ratio as decimal(10,2))
-	, avgprice
-	, medianprice
+select 	zipcode SERIAL PRIMARY KEY
+	, cast(ratio as decimal(10,2)) INT
+	, avgprice INT
+	, medianprice INT
 FROM pricetorentratio;
 
 select *
