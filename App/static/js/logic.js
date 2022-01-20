fc4946aafbdf9f6976d99bc813a4ce9a604248cc
junki-ogawa <90418741+junki-ogawa@users.noreply.github.com>
2022-01-19 20:02:55 -0700

    update: junkis sloppy js code without lon/lat

--- App/static/js/logic.js
+++ App/static/js/logic.js
@@ -1,17 +1,65 @@
-// async function getHello() {
-//     const url = 'http://127.0.0.1:5000/api/v1.0/data'
-//     fetch(url)
-//     .then(response => response.json())  
-//     .then(json => {
-//         console.log(json);
-//         document.getElementById("demo").innerHTML = JSON.stringify(json)
-//     createFeatures(data);
-//     })
-// }
-// getHello();
-// //https://stackoverflow.com/questions/57891275/simple-fetch-get-request-in-javascript-to-a-flask-server
+async function getHello(){
+    const dataResponse = await fetch("/data")
+    const rentData = await dataResponse.json();
+    console.log(rentData)
 
-// function createFeatures(data) {
+    const markers = [];
+
+////////TO CREATE THE MARKERS/// Loop through the Markers (city) array.
+    for (let index = 0; index < rentData.length; index++) {
+        let markers = rentData[index]
+    // For each station, create a marker, and bind a popup with the station's name.
+        let zipMarker = L.marker([rentData.lat, rentData.lon])
+        .bindPopup("<h3>" + rentData.city + 
+        "<h3><h3>Population: " + rentData.population + 
+        "<h3><h3>Median Sale Price: " + rentData.medianprice +
+        "<h3><h3>Median Monthly Rent Price: " + rentData.avgprice +
+        "<h3><h3>Price to Rent Ratio: " + rentData.ratio +
+        "<h3>")
+    // Add the marker to the bikeMarkers array.
+    markers.push(markers)
+    }
+    // Create a layer group that's made from the markers (the zips/cities) array
+    markersLayer = L.layerGroup(markers)
+
+/////////CREATE HEATMAP OF THE RATIO/// Loop through the RATIO array.
+    var heatArray = [];
+
+    for (let index = 0; index < rentData.length; index++) {
+        let ratio = rentData[index]
+    
+        if (ratio) {
+            heatArray.push([ratio.coordinates[1], ratio.coordinates[0]]);
+        }
+    }
+
+////////////ADDING THE MAPS/////////
+    var heatMap = L.heatLayer(heatArray, {
+        radius: 20,
+        blur: 35
+    }).addTo(map);
+
+    var markerMap = {
+        "Zipcodes": markersLayer,
+    };
+
+    var map = L.map("map", {
+        center: [37.09, -95.71],
+        zoom: 5
+    });
+
+    // Add a tile layer.
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    L.control.layers(baseMaps, markerMap, {
+        collapsed: false
+    }).addTo(map);
+
+
+
+// function createFeatures(rentData) {
 //     function onEachFeature(feature, layer) {
 //         layer.bindPopup(`<h3>${feature.properties.place}</h3><hr><p>${new Date(feature.properties.time)}</p>`);
 //     }
@@ -26,12 +74,5 @@
 // }
 
 
-
-// ;
-
-async function getHello(){
-    const dataResponse = await fetch("/data")
-    const rentData = await dataResponse.json();
-    console.log(rentData)
 }
 getHello();
